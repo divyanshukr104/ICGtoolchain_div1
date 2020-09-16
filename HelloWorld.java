825bad771e90435b6a4d2e14e395c44d97cfb1c7
divyanshukr104 <divyanshukr104@gmail.com>
2020-09-16 11:37:12 +0000

    new helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! Citi, experiment 1"); 
-    }
+// Simple Java Program to Print Hello World
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
+
